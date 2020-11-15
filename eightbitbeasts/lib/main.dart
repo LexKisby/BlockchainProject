cfb75421a5ff42acf596ef4420a5506dcc818e1e
LexKisby <kisbyalexander@gmail.com>
2020-11-15 14:53:32 -0800

    Established basic app ui. need to look into web3dart next

--- eightbitbeasts/lib/main.dart
+++ eightbitbeasts/lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:web3dart/web3dart.dart'
+import 'package:web3dart/web3dart.dart';
 
 void main() {
   runApp(MyApp());
@@ -10,7 +10,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: '8Bit Beasts',
       theme: ThemeData(
         // This is the theme of your application.
         //
@@ -23,92 +23,154 @@ class MyApp extends StatelessWidget {
         // is not restarted.
         primarySwatch: Colors.blue,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Root(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
+class Root extends StatefulWidget {
   @override
-  _MyHomePageState createState() => _MyHomePageState();
+  RootState createState() => RootState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
+class RootState extends State<Root> {
+  int _selectedIndex = 0;
+  PageController _controller = PageController(
+    initialPage: 0,
+  );
 
-  void _incrementCounter() {
+  @override 
+  void dispose(){
+    _controller.dispose();
+    super.dispose();
+  }
+
+  void _onItemTapped(int index) {
     setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
+      _selectedIndex = index;
     });
   }
 
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
+    return Scaffold(
+      body: PageView(
+        controller: _controller,
+        children: [
+          InventoryPage(),
+          AuctionPage(),
+          CapturePage(),
+          HatchingPage(),
+          SettingsPage()
+        ],
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+          icon: Icon(Icons.library_books),
+          label: "Inventory"
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.gavel),
+            label: "Auctions"
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.security),
+            label: "Capture"
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.flare),
+            label: "Hatch"
+          ),
+
+          BottomNavigationBarItem(
+            icon: Icon(Icons.settings_applications),
+            label: "Settings"
+          )
+
+        ],
+        currentIndex: _selectedIndex,
+        selectedItemColor: Colors.redAccent[700],
+        unselectedItemColor: Colors.grey,
+        onTap: _onItemTapped,
+        type: BottomNavigationBarType.fixed,
+      ),
+    );
+  }
+
+/*
+  @override 
+  Widget build(BuildContext context) {
+    return PageView(
+      controller: _controller,
+      children: [
+        InventoryPage(),
+        AuctionPage(),
+        CapturePage(),
+        HatchingPage(),
+        SettingsPage()
+      ],
+    );
+  }*/
+}
+
+class InventoryPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text("Inventory"),),
+      body: Center(child: Text("yo"))
+    );
+    
+  }
+} 
+
+class AuctionPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+    appBar: AppBar(
+      title: Text("Market")
+    ),
+    body: Center(child: Text("sup"))
+    );
+  }
+}
+
+class CapturePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+      title: Text("Capture")
+    ),
+    body: Center(child: Text("cool beans"))
+    );
+  }
+}
+
+class HatchingPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      title: Text("Hatch")
+    ), body: Center(child: Text("whooop"))
+    );
+  }
+}
+
+class SettingsPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+      title: Text("Settings")
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      body: Center(child: Text("hmm"))
     );
   }
 }
