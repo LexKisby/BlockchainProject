b24b5f8705bbb303cc7f1e2615d96802114475cb
LexKisby <kisbyalexander@gmail.com>
2021-04-09 20:28:10 +0100

    errors removed from contract

--- eightbitbeasts/lib/contracts/interfaced/full.sol
+++ eightbitbeasts/lib/contracts/interfaced/full.sol
@@ -1558,12 +1558,21 @@ contract Market is Owner {
     }
 }
 
-
 contract Dungeon is Owner {
     uint256 minRecoveryPeriod = 1 days;
 
-    event DungeonChallenged(uint256 beast1, uint256 beast2, address tamer, uint difficulty);
-    event DungeonFailed(uint256 beast1, uint256 beast2, address tamer, uint difficulty);
+    event DungeonChallenged(
+        uint256 beast1,
+        uint256 beast2,
+        address tamer,
+        uint256 difficulty
+    );
+    event DungeonFailed(
+        uint256 beast1,
+        uint256 beast2,
+        address tamer,
+        uint256 difficulty
+    );
 
     address MotherAddress = 0x06012c8cf97BEaD5deAe237070F9587f8E7A266d;
     MotherSetter MotherContract = MotherSetter(MotherAddress);
@@ -1571,44 +1580,70 @@ contract Dungeon is Owner {
     function updateMotherAddress(address _address) external isOwner() {
         MotherAddress = _address;
         MotherContract = MotherSetter(MotherAddress);
-
     }
-//#####################################################
+
+    //#####################################################
 
     //internal functions
-    _calculateStrength(MotherSetter.Beast memory _b1, MotherSetter.Beast memory _b2) internal pure returns (uint) {
-        uint s1 = _b1.attackSpeed * (_b1.primaryDamage + _b1.secondaryDamage) * _b1.accuracy + _b1.resistance + _b1.evasion * _b1.constitution + _b1.hp;
-        uint s2 = _b2.attackSpeed * (_b2.primaryDamage + _b2.secondaryDamage) * _b2.accuracy + _b2.resistance + _b2.evasion * _b2.constitution + _b2.hp;
-        return s1+s2;
+    function _calculateStrength(
+        MotherCore.Beast memory _b1,
+        MotherCore.Beast memory _b2
+    ) internal pure returns (uint256) {
+        uint256 s1 =
+            _b1.stats.attackSpeed *
+                (_b1.stats.primaryDamage + _b1.stats.secondaryDamage) *
+                _b1.stats.accuracy +
+                _b1.stats.resistance +
+                _b1.stats.evasion *
+                _b1.stats.constitution +
+                _b1.stats.hp;
+        uint256 s2 =
+            _b2.stats.attackSpeed *
+                (_b2.stats.primaryDamage + _b2.stats.secondaryDamage) *
+                _b2.stats.accuracy +
+                _b2.stats.resistance +
+                _b2.stats.evasion *
+                _b2.stats.constitution +
+                _b2.stats.hp;
+        return s1 + s2;
     }
 
-    _rewardBand(uint _s, uint _d) internal view returns (uint minR, uint rangeR, uint minE, uint rangeE) {
+    function _rewardBand(uint256 _s, uint256 _d)
+        internal
+        view
+        returns (
+            uint256 minR,
+            uint256 rangeR,
+            uint256 minE,
+            uint256 rangeE
+        )
+    {
         if (_d > 5) {
             if (_d > 7) {
                 if (_d == 8) {
                     if (_s < 250000000) {
-                        return (0,0,0,0);
+                        return (0, 0, 0, 0);
                     }
                 }
                 if (_d == 9) {
                     if (_s < 1000000000) {
-                        return (0,0,0,0);
+                        return (0, 0, 0, 0);
                     }
                 }
                 if (_d == 10) {
                     if (_s < 1750000000) {
-                        return (0,0,0,0);
+                        return (0, 0, 0, 0);
                     }
                 }
             } else {
                 if (_d == 6) {
                     if (_s < 2000000) {
-                        return (0,0,0,0);
+                        return (0, 0, 0, 0);
                     }
                 }
                 if (_d == 7) {
                     if (_s < 40000000) {
-                        return (0,0,0,0);
+                        return (0, 0, 0, 0);
                     }
                 }
             }
@@ -1616,51 +1651,79 @@ contract Dungeon is Owner {
             if (_d > 3) {
                 if (_d == 4) {
                     if (_s < 10000) {
-                        return (0,0,0,0);
+                        return (0, 0, 0, 0);
                     }
                 }
                 if (_d == 5) {
                     if (_s < 200000) {
-                        return (0,0,0,0);
+                        return (0, 0, 0, 0);
                     }
                 }
             } else {
                 if (_d == 1) {}
                 if (_d == 2) {
                     if (_s < 300) {
-                        return (0,0,0,0);
+                        return (0, 0, 0, 0);
                     }
                 }
                 if (_d == 3) {
                     if (_s < 1000) {
-                        return (0,0,0,0);
+                        return (0, 0, 0, 0);
                     }
                 }
             }
         }
-        uint minR = _s / 2**(d+1);
-        uint rangeR = 2 ** _d * _d**3;
-        if (minR > 5**d * 2);
-        uint minE = 0;
-        uint rangeE = 0;
+        minR = _s / 2**(_d + 1);
+        rangeR = 2**_d * _d**3;
+        if (minR > 5**_d * 2) {
+            minR = 5**_d * 2;
+        }
+        minE = 0;
+        rangeE = 0;
         if (_d > 1) {
-            rangeE = 2 ** (_d - 1);
+            rangeE = 2**(_d - 1);
         }
         return (minR, rangeR, minE, rangeE);
     }
 
-    _calcReward(uint _s, uint _d) internal view returns (uint R, uint E) {
-        uint minR, uint rangeR, uint minE, uint rangeE = _rewardBand(_s, _d);
-        uint R = minR + (uint(keccak256(abi.encodePacked(block.timestamp))) % 100) * rangeR / 100;
-        uint E = minE + (uint(keccak256(abi.encodePacked(block.timestamp))) % 100) * rangeE / 100;
+    function _calcReward(uint256 _s, uint256 _d)
+        internal
+        view
+        returns (uint256 R, uint256 E)
+    {
+        uint256 minR;
+        uint256 rangeR;
+        uint256 minE;
+        uint256 rangeE;
+        (minR, rangeR, minE, rangeE) = _rewardBand(_s, _d);
+        R =
+            minR +
+            ((uint256(keccak256(abi.encodePacked(block.timestamp))) % 100) *
+                rangeR) /
+            100;
+        E =
+            minE +
+            ((uint256(keccak256(abi.encodePacked(block.timestamp))) % 100) *
+                rangeE) /
+            100;
 
         return (R, E);
     }
 
     //external function:
-    function enterDungeon(uint256 _beastId1, uint256 beastId2, uint _dungeonLvl) external {
-        require(MotherContract.beastToTamer(_beastId1) == msg.sender, "You do not own beast 1");
-        require(MotherContract.beastToTamer(_beastId2) == msg.sender, "You do not own beast 2");
+    function enterDungeon(
+        uint256 _beastId1,
+        uint256 _beastId2,
+        uint32 _dungeonLvl
+    ) external {
+        require(
+            MotherContract.beastToTamer(_beastId1) == msg.sender,
+            "You do not own beast 1"
+        );
+        require(
+            MotherContract.beastToTamer(_beastId2) == msg.sender,
+            "You do not own beast 2"
+        );
 
         //based on level to adjust difficulty, takes two beasts and then computes a reward
         MotherSetter.Beast memory beast1 = MotherContract.getBeast(_beastId1);
@@ -1669,29 +1732,36 @@ contract Dungeon is Owner {
         require(beast2.readyTime <= block.timestamp, "Beast 2 is not ready");
 
         //assess strength
-        uint s = _calculateStrength(beast1, beast2);
-        
+        uint256 s = _calculateStrength(beast1, beast2);
+
         //get reward
-        uint r, uint e = _calcReward(s, _d);
-        uint xp = _d;
-        if (r>0) {
-            xp = _d * 10;
+        uint256 r;
+        uint256 e;
+        (r, e) = _calcReward(s, _dungeonLvl);
+        uint32 xp = _dungeonLvl;
+        if (r > 0) {
+            xp = _dungeonLvl * 10;
         }
 
         //give reward to tamer
-        MotherContract.depositCurrency(int(e), msg.sender, 0);
-        MotherContract.depositCurrency(int(r), msg.sender, 1);
+        MotherContract.depositCurrency(int256(e), msg.sender, 0);
+        MotherContract.depositCurrency(int256(r), msg.sender, 1);
         //give xp
-        MotherContract.addXp(_beastId1, _d);
-        MotherContract.addXp(_beastId2, _d);
+        MotherContract.addXp(_beastId1, xp);
+        MotherContract.addXp(_beastId2, xp);
         //rest them
-        MotherContract.triggerRecoveryPeriod(_beast1, 1);
-        MotherContract.triggerRecoveryPeriod(_beast2, 1);
+        MotherContract.triggerRecoveryPeriod(_beastId1, 1);
+        MotherContract.triggerRecoveryPeriod(_beastId2, 1);
 
-        if (r>0){
-        emit DungeonChallenged(_beast1, _beast2, msg.sender, _d);
+        if (r > 0) {
+            emit DungeonChallenged(
+                _beastId1,
+                _beastId2,
+                msg.sender,
+                _dungeonLvl
+            );
         } else {
-            emit DungeonFailed(_beast1, _beast2, msg,sender, _d);
+            emit DungeonFailed(_beastId1, _beastId2, msg.sender, _dungeonLvl);
         }
     }
-} 
+}
